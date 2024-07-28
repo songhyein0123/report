882a48598ae6455e2ade599ce65ae82ac489bc9c
t2023-m0125 <t2023-m0125@t2023-m0125ui-MacBookAir.local>
2024-07-29 00:48:43 +0900

    기본틀

--- movie.js
+++ movie.js
@@ -1,1 +1,18 @@
-alert("movie.js 연결완료")
+
+const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: 
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDNjYjM2MzlhMDUzZWY2YzBjZDZiODQyMGY5MTc2MSIsIm5iZiI6MTcyMjEzNzk1Mi45NTExNjUsInN1YiI6IjY2YTViYWVlOWMyZGZiZWM5MzMwMjIzZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-ViB8sOJGi2FNCe45N4I4jWvQV5Z147MwjuJ_QwsU1M"
+    }
+  };
+  
+  fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1", options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+    function hey() {
+      alert('영화id : ')
+    }
